4fd374cddef592cdd3273ba488ffdd3d087ad4c8
My Name <dev62ru@gmail.com>
2019-04-24 21:03:53 +0300

    home

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+//use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+   // public function boot() { Schema::getFacadeApplication(); }
 
     /**
      * Register any application services.
