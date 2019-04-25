b6f670fcf58cad677b9e9172c4fdc251c1eeafe3
Sergey62 <sergey62.13@yandex.ru>
2019-04-25 15:32:55 +0300

    table

--- resources/views/autoService.blade.php
+++ resources/views/autoService.blade.php
@@ -5,20 +5,49 @@
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="/public/bootstrap4/css/bootstrap.css" rel="stylesheet">
+	{{--<link href="/public/css/about.css" rel="stylesheet">--}}
+
+	<script src="/public/js/jquery-3.4.0.js"></script>
+	<script src="/public/bootstrap4/js/bootstrap.js"></script>
+	{{--<script src="/public/js/about.js"></script>--}}
 	<title>AutoService</title>
 </head>
 <body>
 	<div class="container">
 		<div class="row">
 			<h1>Автосервис "Южный порт"</h1>
-			<div>
+			{{--<div>
 				<i>Разрабатывается</i>
-			</div>
+			</div>--}}
 		</div>
+		<table class="table table-striped table-bordered table-hover">
+			<thead>
+				<tr>
+					<th scope="col">Услуга</th>
+					<th scope="col">Описание</th>
+					<th scope="col">Цена</th>
+				</tr>
+			</thead>
+			<tbody class="">
+				<tr>
+					<td>Ремонт двигателя</td>
+					<td>Ремонт двигателей любой сложности</td>
+					<td>5000 р.</td>
+				</tr>
+				<tr>
+					<td>Ремонт коробки</td>
+					<td>Ремонт коробок АПП, Автомат любой сложности</td>
+					<td>30000 р.</td>
+				</tr>
+			</tbody>
+		</table>
 	</div>
 </body>
+{{--
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+--}}
 
 </html>
